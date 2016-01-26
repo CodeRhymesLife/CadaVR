466401f82b8a0f6db6fbf5c314df863698bde742
Ryan James <rcjames1004@gmail.com>
2016-01-25 23:58:36 -0800

    Update server port

--- server.js
+++ server.js
@@ -3,11 +3,7 @@
 const Hapi = require('hapi');
 
 // Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
+var server = new Hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
 
 // Add the route
 server.register(require('inert'), (err) => {
