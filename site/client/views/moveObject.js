b2a6377a160fd6543ff9a569de9da84eff1a0b24
Ryan James <rcjames1004@gmail.com>
2016-01-30 01:53:20 -0800

    Use THREE.SceneUtils to attach and detach when dragging

--- site/client/views/moveObject.js
+++ site/client/views/moveObject.js
@@ -4,24 +4,21 @@ Template.moveObject.onRendered(function () {
 	var camera = $("a-scene").get(0).cameraEl;
 
     $(".moveableObject").mousedown(function (e) {
+		if(dragging != null)
+			return;
+
 		dragging = $( this ).get(0);
 
-		// Get the offset between the object and the camera
-		var offset = new THREE.Vector3().copy(camera.getAttribute("position")).sub(dragging.getAttribute("position"));
-		
-		// Add the object as a child of the camera so it follows the camera
-        camera.add(dragging);
-		
-		// Move the object dirrectly in front of the camera
-		dragging.setAttribute("position", {x: 0, y: 0, z: -offset.z})
+		dragging.object3D.parent.updateMatrixWorld();
+		THREE.SceneUtils.attach( dragging.object3D, scene.object3D, camera.object3D );
     });
 	
 	$(".moveableObject").mouseup(function (e) {
 		if(dragging == null || dragging != $( this ).get(0))
 			return;
-		
-		camera.object3D.updateMatrixWorld();
-		THREE.SceneUtils.detach( dragging.object3D, camera.object3D, scene.object3D );
+
+		dragging.object3D.parent.updateMatrixWorld();
+		THREE.SceneUtils.detach( dragging.object3D, dragging.object3D.parent, scene.object3D );
 		dragging = null;
     });
 });
