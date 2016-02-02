dca86d46eb7724dc9a0d825cfcf13d24266d8852
Ryan James <rcjames1004@gmail.com>
2016-02-02 01:52:22 -0800

    Show user tests when slides open

--- site/client/views/pitch.js
+++ site/client/views/pitch.js
@@ -10,10 +10,30 @@ Template.pitchWatch1stPerson.onRendered(function () {
 });
 
 function loadPitch () {
+	var showUserTest = function (selector) {
+		return function () {
+			$( selector ).get(0).setAttribute("visible", true);
+		}
+	}
+	
 	var slides = [
-		"vrlearning.png",
-		"cadavr.png",
-		"team.png",
+		{
+			image: "vrlearning.png",
+		},
+		{
+			image: "cadavr.png",
+		},
+		{
+			image: "menuSelection.png",
+			onLoad: showUserTest(".menuSelectionContainer")
+		},
+		{
+			image: "objectInteraction.png",
+			onLoad: showUserTest(".objectInteractionContainer")
+		},
+		{
+			image: "team.png",
+		},
 	];
 	
 	var currentSlide = 0;
@@ -22,9 +42,15 @@ function loadPitch () {
 		if(slideIndex < 0 || slideIndex > slides.length - 1)
 			return;
 
+		var slideImage = slides[slideIndex].image;
+		var onLoad = slides[slideIndex].onLoad;
+		
 		var slidesEl = $(".slideShow a-image.slides").get(0);
 		var oldSrc = slidesEl.getAttribute("src");
-		slidesEl.setAttribute("src", "images/pitch/" + slides[slideIndex]);
+		slidesEl.setAttribute("src", "images/pitch/" + slideImage);
+		
+		if(onLoad)
+			onLoad();
 		
 		// Non components don't fire events when attributes change, so we need to do it ourselves
 		slidesEl.emit('attrchanged', {
