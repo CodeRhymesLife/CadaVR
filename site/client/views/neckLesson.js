e4f49104f6384323c4ee65b3d437be8575bcbe41
Ryan James <rcjames1004@gmail.com>
2016-02-09 22:56:16 -0800

    Add rotation

--- site/client/views/neckLesson.js
+++ site/client/views/neckLesson.js
@@ -1,3 +1,19 @@
 Template.neckLesson.onRendered(function () {
-	
+	$(".leftarrow").click( function () {
+		rotateBody(-30)
+	});
+
+	$(".rightarrow").click( function () {
+		rotateBody(30)
+	});
 });
+
+function rotateBody(amount) {
+	var bodyElement = $(".human").get(0);
+	var rotation = bodyElement.getAttribute("rotation") || { x: 0, y: 0, z: 0 };
+	rotation.y -= amount;
+	bodyElement.setAttribute("rotation",
+		String(rotation.x) + " " +
+		String(rotation.y) + " " +
+		String(rotation.z));
+}
